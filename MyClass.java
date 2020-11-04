fb42abfbd6c416f4dccb3b8a1207345457d0531c
叶宝平 <7664941+yabcd@user.noreply.gitee.com>
2020-11-04 12:04:55 +0800

    opg

--- MyClass.java
+++ MyClass.java
@@ -1,127 +1,125 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class MyClass {
-    private char CHAR;
-    private boolean newChar;
-    private String TOKEN;
-    private String SY;
-    private Reader reader;
-
-    public MyClass(String path){
-        newChar = true;
-        File file = new File(path);
-        try {
-            reader = new InputStreamReader(new FileInputStream(file));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public  int getChar(){
-        if(!newChar){
-            newChar=true;
-            return 0;
-        }
-        try {
-            int res=0;
-            if((res = reader.read())==-1){
-                return -1;
+    public int analyseLine(char[] line) {
+        char[] stack = new char[1000];
+        int sp = 0;
+        stack[sp++] = '#';
+        int read = 0;
+        while (stack[1] != '#') {
+            int fsp = getvtIndex(stack, sp);
+            if (stack[fsp] == '#' && line[read] == '#') return 0;
+            //无法比较，终止运行
+            if (compare(stack[fsp], line[read]) == null) {
+                System.out.println("E");
+                return 0;
             }
-            CHAR = (char)res;
-        } catch (IOException e) {
-            e.printStackTrace();
+            //栈内优先级小，进行移入
+            if (compare(stack[fsp], line[read]) <= 0) {
+                System.out.println("I" + line[read]);
+                stack[sp++] = line[read++];
+                continue;
+            } else {
+                int x = getvtIndex(stack, fsp);
+                while (compare(stack[x], stack[fsp]) == 0) {
+                    fsp = x;
+                    if ((x = getvtIndex(stack, fsp)) == -1) break;
+                }
+                if ((x = getvtIndex(stack, fsp)) != -1) fsp = x;
+                rule(stack, fsp, sp);
+                sp = fsp + 2;
+                stack[sp - 1] = 'N';
+//                for(int i=0;i<sp;i++){
+//                    System.out.print(" "+stack[i]);
+//                }
+//                System.out.println();
+            }
         }
         return 0;
     }
-    public int getNBC(){
-        while(true){
-            int res = 0;
-            if((res=getChar())==-1) return -1;
-            if(!Character.isSpaceChar(CHAR) && CHAR!='\r'&&CHAR!='\n'&&CHAR!='\t') return 0;
-        }
+
+    public void rule(char[] stack, int fsp, int sp) {
+        if (sp - fsp == 4) {
+            System.out.println("R");
+        } else if (sp - fsp == 2) {
+            if (stack[sp - 1] != 'i')
+                System.out.println("RE");
+            else
+                System.out.println("R");
+        } else
+            System.out.println("RE");
     }
 
-    public void CAT(){
-        TOKEN+=CHAR;
+    public int getvtIndex(char[] stack, int sp) {
+        if (sp == 0) return -1;
+        if (sp == 1 && !isVt(stack[sp - 1])) return -1;
+        if (isVt(stack[sp - 1])) return sp - 1;
+        return sp - 2;
     }
 
-    public boolean isLetter(){
-        return Character.isLetter(CHAR);
-    }
-    public boolean isDigit(){
-        return Character.isDigit(CHAR);
-    }
-    public void unGetCh(){
-        newChar = false;
-    }
-    public boolean reserve(){
-        if(TOKEN.equals("BEGIN")){
-            SY = "Begin";
-            return true;
-        }else if(TOKEN.equals("END")){
-            SY = "End";
-            return true;
-        }else if(TOKEN.equals("FOR")){
-            SY = "For";
-            return true;
-        }else if(TOKEN.equals("IF")){
-            SY = "If";
-            return true;
-        }else if(TOKEN.equals("THEN")){
-            SY = "Then";
-            return true;
-        }else if(TOKEN.equals("ELSE")){
-            SY = "Else";
-            return true;
+    public boolean isVt(char s) {
+        switch (s) {
+            case '+':
+            case '*':
+            case '(':
+            case ')':
+            case 'i':
+            case '#':
+                return true;
         }
         return false;
     }
-    public int ATOI(){
-        return Integer.valueOf(TOKEN);
-    }
 
-    public void ERROR(){
-        System.out.println("Unknown");
-        System.exit(0);
-    }
-    public void run(){
-        TOKEN = "";
-        boolean ifrun=true;
-        while(getNBC()!=-1&&ifrun){
-            if(isLetter()){
-                do{
-                    CAT();
-                    if(getChar()==-1){ifrun=false;break;}
-                }while(isLetter()||isDigit());
-                unGetCh();
-                if(reserve()) System.out.println(SY);
-                else System.out.println("Ident("+TOKEN+")");
-            }else if(isDigit()){
-                do{
-                    CAT();if(getChar()==-1){ifrun=false;break;}
-                }while(isDigit());
-                unGetCh();
-                System.out.println("Int("+ATOI()+")");
-            }else if(CHAR=='+') System.out.println("Plus");
-            else if(CHAR=='*') System.out.println("Star");
-            else if(CHAR==',') System.out.println("Comma");
-            else if(CHAR=='(') System.out.println("LParenthesis");
-            else if(CHAR==')') System.out.println("RParenthesis");
-            else if(CHAR==':') {
-                if(getChar()==-1){ifrun=false;}
-                if(CHAR=='=') System.out.println("Assign");
-                else{
-                    unGetCh();
-                    System.out.println("Colon");
-                }
-            }
-            else ERROR();
-            TOKEN = "";
+    public int f(char s) {
+        switch (s) {
+            case '+':
+                return 2;
+            case '*':
+                return 4;
+            case '(':
+                return 0;
+            case ')':
+                return 6;
+            case 'i':
+                return 6;
+            case '#':
+                return 0;
         }
+        throw new RuntimeException("this vt does not exist");
     }
 
-    public static void main(String[] args) {
-        MyClass a = new MyClass(args[0]);
-        a.run();
+    public int g(char s) {
+        switch (s) {
+            case '+':
+                return 1;
+            case '*':
+                return 3;
+            case '(':
+                return 5;
+            case ')':
+                return 0;
+            case 'i':
+                return 5;
+            case '#':
+                return 0;
+        }
+        throw new RuntimeException("this vt does not exist");
+    }
+
+    public Integer compare(char s1, char s2) {
+        if (s1 == 'i' && (s2 == 'i' || s2 == '(')) return null;
+        if (s1 == '(' && s2 == '#') return null;
+        if (s1 == ')' && (s2 == 'i' || s2 == '(')) return null;
+        if (s1 == '#' && (s2 == ')' || s2 == '#')) return null;
+        return f(s1) - g(s2);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        MyClass myClass = new MyClass();
+        Scanner sc = new Scanner(new File("C:\\Users\\hp\\IdeaProjects\\analyse\\test1.txt"));
+        while (sc.hasNext()) {
+            myClass.analyseLine((sc.nextLine() + "#").toCharArray());
+        }
     }
 }
